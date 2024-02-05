65c1eda289c139b84b146abf2050ac2aa7b147b2
Eduardo Alvarez <opticoedu@gmail.com>
2024-02-05 14:41:05 -0300

    almost done

--- src/components/Gatos/index.jsx
+++ src/components/Gatos/index.jsx
@@ -1,13 +1,21 @@
-
+import { WrapperVideo, Text } from './styles.js'
+import { Link } from 'react-router-dom'
 
 export function Gatos(){
   return(
   <>
-  
-  <iframe width="866" height="487" src="https://www.youtube.com/embed/ByRSxAqArzQ"
+   <WrapperVideo>
+   <iframe width="866" height="487" src="https://www.youtube.com/embed/ByRSxAqArzQ"
    title="GATOS TIERNOS BEBES 🔵 GATOS LINDOS 🔵 GATOS ADORABLES 🔵 GATITOS GRACIOSISIMOS"
     frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
      allowfullscreen></iframe>
+
+   </WrapperVideo>
+
+   <Link to="/" style={{textDecoration: 'none'}}>
+          <Text>&laquo; Voltar</Text>
+       </Link> 
+  
        
   </>
   )
---
+++ src/components/Gatos/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const WrapperVideo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 450px;
+    margin-top: 70px;
+`;
+
+export const Text = styled.a`
+  display: flex;
+  margin-top: 80px;
+  color: black;
+  text-align: center;
+  justify-content: center;
+  font-size: 25px
+`;
